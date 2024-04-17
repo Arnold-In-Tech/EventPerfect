6abf8acfd7e2348bdfddef3cd1d58baf7976e72b
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-04-17 13:15:07 +0300

    Worked on the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-// import LoginUser from './components/SignIn';
-import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
+import AllEvents from "./components/AllEvents";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
     <>
-    <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/Login" element={<LoginUser />} /> */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/events" element={<AllEvents />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Routes>
       <Footer />
     </>
