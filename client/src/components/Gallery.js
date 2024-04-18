fb5466d28b1ae804cb952eb22ea4ecbd17d54dc1
samwanyua <samexample8@gmail.com>
2024-04-18 21:36:28 +0300

    Added Landing page, Gallery section

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -20,10 +20,11 @@ function Gallery() {
     "https://images.unsplash.com/photo-1507209696998-3c532be9b2b5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    
+
 
     
   ];
+  
 
   return (
     <div className="bg-gray-100 py-12">
