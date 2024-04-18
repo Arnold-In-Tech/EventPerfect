68c5e956bdb5257a9b176438a9f9acc9e02baf08
samwanyua <samexample8@gmail.com>
2024-04-18 15:25:10 +0300

    Added new componets

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Dashboard from "./components/Dashboard";
 import AllEvents from "./components/AllEvents";
 import Reviews from "./components/Reviews";
 import Login from "./components/Login";
+import Create_event from "./components/Create_event";
+import My_events from "./components/My_events";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/events" element={<AllEvents />} />
+        <Route path="/create_event" element={<Create_event />} />
+        <Route path="/my_events" element={<My_events />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ client/src/components/Create_event.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Create_event() {
+  return (
+    <div>Create_event</div>
+  )
+}
+
+export default Create_event
